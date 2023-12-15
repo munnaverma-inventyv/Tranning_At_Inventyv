4a2b7b5f3c88c2609f1112cdcdadb0117e31fec2
mv-here <munnavermainventyv@gmail.com>
2023-12-15 22:19:28 +0530

    Assignment 02 completed

---
+++ Assignment_02/assets/js/main.js
@@ -0,0 +1,33 @@
+(() => {
+  var arr1 = [1, 2, 3, 4, 5];
+  var FirstElement = arr1.shift();
+  func2(FirstElement, arr1);
+})();
+
+function func2(FirstElement, arr1) {
+  var arr2 = [6, 7, 8, 9];
+  arr2.unshift(FirstElement);
+  for (let i = 0; i < arr1.length; i++) {
+    arr2.push(arr1[i]);
+  }
+  var sum = 0;
+  for (let i = 0; i < arr2.length; i++) {
+    sum = sum + arr2[i];
+  }
+  let promise = new Promise((resolve, reject) => {
+    if (sum > 30) {
+      resolve(sum);
+    } else {
+      reject(error);
+    }
+  });
+  promise.then((result) => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((response) => response.json())  
+    .then((data)=>{
+        for(let i=0;i<sum;i++){
+            console.log(data[i].url);
+        }
+    })
+  });
+}
